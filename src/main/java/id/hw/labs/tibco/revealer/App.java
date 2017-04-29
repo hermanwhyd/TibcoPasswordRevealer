9b511157f008494c8c88e734cb49ba0a0681b5cd
HermanW <hwahyudi@xl.co.id>
2017-04-29 22:21:40 +0700

    init project

---
+++ src/main/java/id/hw/labs/tibco/revealer/App.java
@@ -0,0 +1,11 @@
+package id.hw.labs.tibco.revealer;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        MainForm mainForm = new MainForm();
+        mainForm.setLocationRelativeTo(null);
+        mainForm.setVisible(true);
+    }
+}
